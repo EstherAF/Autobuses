353fec89ac19b59dcf3d71083ccaa97096ecf61a
Felix Queiruga <felix.queiruga@gmail.com>
2014-08-02 20:06:01 +0200

    Creates an adapter for the Spinners in the search fragment, that uses a list of Stops.

--- app/src/main/java/es/estheraf/horariosbus/fragment/SearchFragment.java
+++ app/src/main/java/es/estheraf/horariosbus/fragment/SearchFragment.java
@@ -5,32 +5,45 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import es.estheraf.horariosbus.R;
+import es.estheraf.horariosbus.adapter.StopsSpinnerAdapter;
+import es.estheraf.horariosbus.data.model.Stop;
 
 public class SearchFragment extends Fragment {
 
-    private Spinner mOrigin;
-    private Spinner mDestination;
+    private Spinner mOrigins;
+    private Spinner mDestinations;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
 
-        List<String> stops = Arrays.asList("Stop 1", "Stop 2");
+        mOrigins = (Spinner) view.findViewById(R.id.origin);
+        mDestinations = (Spinner) view.findViewById(R.id.destination);
 
-        mOrigin = (Spinner) view.findViewById(R.id.origin);
-        mDestination = (Spinner) view.findViewById(R.id.destination);
-        mOrigin.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, stops));
-        mDestination.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, stops));
+        mOrigins.setAdapter(new StopsSpinnerAdapter(getActivity(), getOrigins()));
+        mDestinations.setAdapter(new StopsSpinnerAdapter(getActivity(), getDestinations()));
 
         return view;
+    }
+
+    private List<Stop> getOrigins() {
+        return Arrays.asList(
+                new Stop(3, "Origin 1"),
+                new Stop(4, "Origin 2")
+        );
+    }
+
+    private List<Stop> getDestinations() {
+        return Arrays.asList(
+                new Stop(3, "Destination 1"),
+                new Stop(4, "Destination 2")
+        );
     }
 
 }
