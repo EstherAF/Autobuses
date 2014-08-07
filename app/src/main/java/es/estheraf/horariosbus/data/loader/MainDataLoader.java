21a74c727b2276812101e676b1c1173a728707d0
Esther <alvarezfeijoo.esther@gmail.com>
2014-08-07 18:16:47 +0200

    IOExceptions handled as LoadingDataException (custom). Use of LogUtil for logging

--- app/src/main/java/es/estheraf/horariosbus/data/loader/MainDataLoader.java
+++ app/src/main/java/es/estheraf/horariosbus/data/loader/MainDataLoader.java
@@ -1,16 +1,15 @@
 package es.estheraf.horariosbus.data.loader;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Collection;
 import java.util.List;
 
 import es.estheraf.horariosbus.data.model.Route;
 import es.estheraf.horariosbus.data.model.Stop;
+import es.estheraf.horariosbus.util.LogUtil;
 
 /**
  * Loader of business data from JSON
@@ -24,14 +23,14 @@ class MainDataLoader {
      */
     private static File sRoutesDataSourceFile = new File("routes.json");
     /**
-     * Reference to stops data file
+     * Reference to routeStops data file
      */
-    private static File sStopsDataSourceName = new File("stops.json");
+    private static File sStopsDataSourceName = new File("routeStops.json");
 
     /**
      * Json mapper
      */
-    private ObjectMapper mapper = new ObjectMapper();
+    private static ObjectMapper mapper = new ObjectMapper();
 
     /**
      * Obtain full list of routes contained in his related file
@@ -39,17 +38,29 @@ class MainDataLoader {
      * @return list of routes
      * @throws IOException Exception parsing JSON file
      */
-    protected List<Route> loadRoutes() throws IOException {
-        return mapper.readValue(sRoutesDataSourceFile, new TypeReference<List<Route>>() {});
+    protected static List<Route> loadRoutes() throws LoadingDataException {
+        try {
+            return mapper.readValue(sRoutesDataSourceFile, new TypeReference<List<Route>>() {
+            });
+        } catch (IOException e){
+            LogUtil.error(e);
+            throw new LoadingDataException();
+        }
     }
 
     /**
-     * Obtain full list of stops contained in his related file
+     * Obtain full list of routeStops contained in his related file
      *
-     * @return list of stops
+     * @return list of routeStops
      * @throws IOException Exception parsing JSON file
      */
-    protected List<Stop> loadStops() throws IOException {
-        return mapper.readValue(sStopsDataSourceName, new TypeReference<List<Stop>>() {});
+    protected static List<Stop> loadStops() throws LoadingDataException {
+        try {
+            return mapper.readValue(sStopsDataSourceName, new TypeReference<List<Stop>>() {
+            });
+        } catch (IOException e){
+            LogUtil.error(e);
+            throw new LoadingDataException();
+        }
     }
 }
