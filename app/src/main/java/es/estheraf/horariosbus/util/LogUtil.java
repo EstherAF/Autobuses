a2fc4869e59c86a3657d4ff00e736e211d1d4cba
Esther <alvarezfeijoo.esther@gmail.com>
2014-08-07 22:34:42 +0200

    Dependencies unused removed

--- app/src/main/java/es/estheraf/horariosbus/util/LogUtil.java
+++ app/src/main/java/es/estheraf/horariosbus/util/LogUtil.java
@@ -2,8 +2,6 @@ package es.estheraf.horariosbus.util;
 
 import android.util.Log;
 
-import java.lang.reflect.Method;
-
 /**
  * Utilities for logging
  *
@@ -16,8 +14,8 @@ public class LogUtil {
      *
      * @return tag
      */
-    public static String getLogTag(){
-        StackTraceElement trace = Thread.currentThread().getStackTrace()[2];    //get the invoker, not actual
+    protected static String getLogTag() {
+        StackTraceElement trace = Thread.currentThread().getStackTrace()[4];    //get the invoker, not actual
         return trace.getClassName() + ":" + trace.getMethodName();
     }
 
@@ -26,17 +24,17 @@ public class LogUtil {
      *
      * @param e exception
      */
-    public static void error(Exception e){
-        LogUtil.error(e, null);
+    public static void error(Exception e) {
+        Log.e(getLogTag(), "", e);
     }
 
     /**
      * Log on error mode an exception with a custom message
      *
-     * @param e exception
+     * @param e       exception
      * @param message message to log
      */
-    public static void error(Exception e, String message){
+    public static void error(Exception e, String message) {
         Log.e(getLogTag(), message, e);
     }
 
@@ -45,7 +43,7 @@ public class LogUtil {
      *
      * @param message message to log
      */
-    public static void debug(String message){
-        Log.e(getLogTag(), message);
+    public static void debug(String message) {
+        Log.d(getLogTag(), message);
     }
 }
