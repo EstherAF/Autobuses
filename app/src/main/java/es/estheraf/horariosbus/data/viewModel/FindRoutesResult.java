df270f9d0446bcad0776b84f65dd97e255275fa9
Esther <alvarezfeijoo.esther@gmail.com>
2014-09-14 21:57:10 +0200

    View model for result of a search

--- app/src/main/java/es/estheraf/horariosbus/data/viewModel/FindRoutesResult.java
+++ app/src/main/java/es/estheraf/horariosbus/data/viewModel/FindRoutesResult.java
@@ -5,13 +5,33 @@ import java.util.List;
 import es.estheraf.horariosbus.data.model.Stop;
 
 /**
+ * Represents a search of routes with its results
+ *
  * @author Esther Álvarez
  */
 public class FindRoutesResult {
 
     public Stop origin;
     public Stop destination;
-
     public List<RouteResult> routeResults;
+
+    /**
+     * Default constructor
+     */
+    public FindRoutesResult(){
+    }
+
+    /**
+     * Parametrized constructor
+     *
+     * @param origin departure stop
+     * @param destination destination stop
+     * @param routeResults  list of results
+     */
+    public FindRoutesResult(Stop origin, Stop destination, List<RouteResult> routeResults){
+        this.origin=origin;
+        this.destination=destination;
+        this.routeResults=routeResults;
+    }
 
 }
